ebbc92a445b53ddb65d4c3a32db62150f532cdaa
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-07-04 12:16:27 +0300

    Add missing 'resolve' configuration in webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: path.resolve(__dirname, './src/index.js'),
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leaderboard',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    hot: true,
+  },
+};
