943a4600e93d9bea1f28411ee5d42c12ca05df0f
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-07-05 20:19:33 +0300

    Add  HTML structure for a leaderboard web page

---
+++ src/modules/apiCalls.js
@@ -0,0 +1,30 @@
+const scoreURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/H8F9d3o2Xe5P/scores/';
+
+
+const submitScoreToAPI = async (user, score) => {
+  const response = await fetch(scoreURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      user,
+      score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP Response Status: ${response.status}`);
+  }
+
+  const result = await response.json();
+  return result;
+};
+
+const retrieveScoreFromAPI = async () => {
+  const response = await fetch(scoreURL);
+  const nameAndScore = await response.json();
+  return nameAndScore;
+};
+
+export { submitScoreToAPI, retrieveScoreFromAPI };
